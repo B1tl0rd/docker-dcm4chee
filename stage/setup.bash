571946efa2b9ae9aed8286ba934ce1bf105552f9
Byron Georgantopoulos <byron.georgantopoulos@gmail.com>
2017-06-15 16:41:06 +0300

    Change sourceforgen.net download locations and set ARR version to 3.0.12

--- stage/setup.bash
+++ stage/setup.bash
@@ -7,25 +7,25 @@ mkdir -p $DCM4CHEE_HOME
 cd $DCM4CHEE_HOME
 
 # Download the binary package for DCM4CHEE
-curl -G http://colocrossing.dl.sourceforge.net/project/dcm4che/dcm4chee/2.17.1/dcm4chee-2.17.1-mysql.zip > /stage/dcm4chee-2.17.1-mysql.zip
+curl -G http://netcologne.dl.sourceforge.net/project/dcm4che/dcm4chee/2.18.1/dcm4chee-2.18.1-mysql.zip > /stage/dcm4chee-2.18.1-mysql.zip
 unzip -q /stage/dcm4chee-2.17.1-mysql.zip
 DCM_DIR=$DCM4CHEE_HOME/dcm4chee-2.17.1-mysql
 
 # Download the binary package for JBoss
-curl -G http://colocrossing.dl.sourceforge.net/project/jboss/JBoss/JBoss-4.2.3.GA/jboss-4.2.3.GA-jdk6.zip > /stage/jboss-4.2.3.GA-jdk6.zip
+curl -G http://netcologne.dl.sourceforge.net/project/jboss/JBoss/JBoss-4.2.3.GA/jboss-4.2.3.GA-jdk6.zip > /stage/jboss-4.2.3.GA-jdk6.zip
 unzip -q /stage/jboss-4.2.3.GA-jdk6.zip
 JBOSS_DIR=$DCM4CHEE_HOME/jboss-4.2.3.GA
 
 # Download the Audit Record Repository (ARR) package
-curl -G http://colocrossing.dl.sourceforge.net/project/dcm4che/dcm4chee-arr/3.0.11/dcm4chee-arr-3.0.11-mysql.zip > /stage/dcm4chee-arr-3.0.11-mysql.zip
-unzip -q /stage/dcm4chee-arr-3.0.11-mysql.zip
-ARR_DIR=$DCM4CHEE_HOME/dcm4chee-arr-3.0.11-mysql
+curl -G http://netcologne.dl.sourceforge.net/project/dcm4che/dcm4chee-arr/3.0.12/dcm4chee-arr-3.0.12-mysql.zip > /stage/dcm4chee-arr-3.0.12-mysql.zip
+unzip -q /stage/dcm4chee-arr-3.0.12-mysql.zip
+ARR_DIR=$DCM4CHEE_HOME/dcm4chee-arr-3.0.12-mysql
 
 # Copy files from JBoss to dcm4chee
 $DCM_DIR/bin/install_jboss.sh jboss-4.2.3.GA > /dev/null
 
 # Copy files from the Audit Record Repository (ARR) to dcm4chee
-$DCM_DIR/bin/install_arr.sh dcm4chee-arr-3.0.11-mysql > /dev/null
+$DCM_DIR/bin/install_arr.sh dcm4chee-arr-3.0.12-mysql > /dev/null
 
 # Install and set up MySQL
 mysql_install_db
